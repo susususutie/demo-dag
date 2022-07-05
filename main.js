7dd809d23d5a7a147d7b342958689ebd98723f36
liuyi <liuyi@mchz.com.cn>
2022-07-05 18:57:25 +0800

    init

---
+++ main.js
@@ -0,0 +1,71 @@
+import { Graph } from "@antv/x6";
+
+const data = {
+  // 节点
+  nodes: [
+    {
+      id: "node1", // String，可选，节点的唯一标识
+      x: 40, // Number，必选，节点位置的 x 值
+      y: 40, // Number，必选，节点位置的 y 值
+      width: 80, // Number，可选，节点大小的 width 值
+      height: 40, // Number，可选，节点大小的 height 值
+      label: "hello", // String，节点标签
+    },
+    {
+      id: "node2", // String，节点的唯一标识
+      x: 160, // Number，必选，节点位置的 x 值
+      y: 40, // Number，必选，节点位置的 y 值
+      width: 80, // Number，可选，节点大小的 width 值
+      height: 40, // Number，可选，节点大小的 height 值
+      label: "world", // String，节点标签
+    },
+    {
+      id: 'node3',
+      shape: 'rect', // 使用 rect 渲染
+      x: 100,
+      y: 200,
+      width: 80,
+      height: 40,
+      label: 'hello',
+    },
+    {
+      id: 'node4',
+      shape: 'ellipse', // 使用 ellipse 渲染
+      x: 300,
+      y: 200,
+      width: 80,
+      height: 40,
+      label: 'world',
+    },
+  ],
+  // 边
+  edges: [
+    {
+      source: "node1", // String，必须，起始节点 id
+      target: "node2", // String，必须，目标节点 id
+    },
+    {
+      source: 'node2',
+      target: 'node3',
+    },
+    {
+      source: 'node3',
+      target: 'node4',
+    },
+  ],
+};
+
+const graph = new Graph({
+  container: document.getElementById("app"),
+  width: 800,
+  height: 600,
+  background: {
+    color: "#fffbe6", // 设置画布背景颜色
+  },
+  grid: {
+    size: 10, // 网格大小 10px
+    visible: true, // 渲染网格背景
+  },
+});
+
+graph.fromJSON(data);
